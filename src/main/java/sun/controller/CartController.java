6d590e0cc86328a14338f1f98fa99cf69bb4e0a7
steven <rjjsteven@163.com>
2020-11-16 23:12:47 +0800

    update file

--- src/main/java/sun/controller/CartController.java
+++ src/main/java/sun/controller/CartController.java
@@ -3,6 +3,7 @@ package sun.controller;
 public class CartController {
     public static void main(String[] args) {
         System.out.println("HK!");
+        System.out.println("HK!");
     }
 }
 
