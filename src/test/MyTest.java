2f9659dd2ef3a0657190865a344e67e97b5dd116
steven <rjjsteven@163.com>
2020-12-23 00:39:02 +0800

    add test

---
+++ src/test/MyTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class MyTest extends ParentTest{
+    @Test
+    public void test1(){
+        System.out.println("hi test");
+    }
+}
---
+++ src/test/ParentTest.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import sun.ConsumerClientApplication;
+
+//@RunWith是JUnit的一个注解, 用来告诉JUnit不要使用内置的方式进行单元测试, 而应该使用指定的类做单元测试 对于Spring单元测试总是要使用 SpringRunner.class
+@RunWith(SpringRunner.class)
+//告诉测试类启动类(这里的DockerspringbootApplication是自己再main/java包下的启动类) 因为启动类加载类配置文件 还有包扫描 才能使用Spring中的bean对象
+@SpringBootTest(classes = ConsumerClientApplication.class)
+public class ParentTest {
+}
+
